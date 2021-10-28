548c6d028c4f3b3c92e006092fc26a8f3d994e3d
Gábor Szanyi <gabor.szanyi@stud.uni-corvinus.hu>
2021-10-28 18:53:24 +0200

    v4

--- UserMaintenance/Week06/Form1.cs
+++ UserMaintenance/Week06/Form1.cs
@@ -7,17 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Week06.Entities;
 using Week06.MnbServiceReference;
 
 namespace Week06
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
         
         public Form1()
         {
             InitializeComponent();
             GetExchangeRates();
+            dataGridView1.DataSource = Rates;
         }
 
         public void GetExchangeRates()
