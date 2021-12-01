4e66577ba2e3cfa6d18b2c451eca642b7dc7a534
Gábor Szanyi <gabor.szanyi@stud.uni-corvinus.hu>
2021-12-01 16:50:18 +0100

    classes

---
+++ UserMaintenance/Week09/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week09.Entities
+{
+    class BirthProbability
+    {
+        public int kor { get; set; }
+        public int darabGyermek { get; set; }
+        public double probBirth { get; set; }
+    }
+}
---
+++ UserMaintenance/Week09/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week09.Entities
+{
+    class DeathProbability
+    {
+        public Gender nem { get; set; }
+        public int kor { get; set; }
+        public double probDeath { get; set; }
+    }
+}
